0e37526fac66a015044b615e734154a7e661af64
brian <liar88828@gmail.com>
2023-05-30 23:43:30 +0700

    make route user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,11 @@
 import { Router } from 'express'
-import { authUser } from "../controller/userController.js";
+import { authUser, logoutUser, profileUser, registerUser, updateProfileUSer } from "../controller/userController.js";
 
 const router = Router()
 
+router.post('/register', registerUser)
 router.post('/auth', authUser)
+router.post('/logout', logoutUser)
+router.route('/profile').get(profileUser).put(updateProfileUSer)
 
 export default router
