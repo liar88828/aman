62e271c65fd4ab6fb96f848685a6f95264bb71c4
brian <liar88828@gmail.com>
2023-06-01 11:41:08 +0700

    register Username

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -1,58 +1,84 @@
-import asyncHandler from 'express-async-handler'
-                                             import UserModel from "../model/userModel.js";
+import asyncHandler from "express-async-handler";
+import UserModel from "../model/userModel.js";
+
+
+
 // description	 user/set token
 // route POST	 	/api/users/auth
 // access 			Public
 const authUser = asyncHandler(
-		async (req, res) => {
-			// res.status(401)
-			// throw  new Error('Something wrong ')
-			res.status(200).json({ message: 'Auth User' })
-		})
-
+  async (req, res) => {
+    // res.status(401)
+    // throw  new Error('Something wrong ')
+    res.status(200).json({ message: "Auth User" });
+  },
+);
 
 // description	 register a new user
 // route 				POST	 	/api/users/register
 // access 			Public
 const registerUser = asyncHandler(
-		async (req, res) => {
+  async (req, res) => {
+    const { name, email, password } = req.body;
 
-			console.log(req.body)
+    const userExist = await UserModel.findOne({ email });
+    if (userExist) {
+      res.status(400);
+      throw new Error("User ready exist");
+    }
 
-			// res.status(401)
-			// throw  new Error('Something wrong ')
-			res.status(200).json({ message: 'Register User' })
-		})
+    const user = await UserModel.create({
+      name,
+      email,
+      password,
+    });
+    if (user) {
+      res.status(200).json({
+        message: "Register User",
+        data: {
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+        },
+      });
+    }else{
+			res.status(400)
+			throw new Error('Invalid User Data')
+		}
+  },
+);
 
 // description	 Logout user
 // route 				POST	 	/api/users/logout
 // access 			Public
 const logoutUser = asyncHandler(
-		async (req, res) => {
-			// res.status(401)
-			// throw  new Error('Something wrong ')
-			res.status(200).json({ message: 'logout User' })
-		})
+  async (req, res) => {
+    // res.status(401)
+    // throw  new Error('Something wrong ')
+    res.status(200).json({ message: "logout User" });
+  },
+);
 
 // description 	GET Profile user
 // route 				POST	 	/api/users/profile
 // access 			Private
 const profileUser = asyncHandler(
-		async (req, res) => {
-			// res.status(401)
-			// throw  new Error('Something wrong ')
-			res.status(200).json({ message: 'Profile User' })
-		})
+  async (req, res) => {
+    // res.status(401)
+    // throw  new Error('Something wrong ')
+    res.status(200).json({ message: "Profile User" });
+  },
+);
 
 // description 	Update Profile user
 // route 				PUT	 	/api/users/profile
 // access 			Private
 const updateProfileUSer = asyncHandler(
-		async (req, res) => {
-			// res.status(401)
-			// throw  new Error('Something wrong ')
-			res.status(200).json({ message: 'Update Profile User' })
-		})
+  async (req, res) => {
+    // res.status(401)
+    // throw  new Error('Something wrong ')
+    res.status(200).json({ message: "Update Profile User" });
+  },
+);
 
-
-export { authUser, logoutUser, profileUser, registerUser, updateProfileUSer }
+export { authUser, logoutUser, profileUser, registerUser, updateProfileUSer };
