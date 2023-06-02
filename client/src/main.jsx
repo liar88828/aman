1a07b64fe7d016e9003a94212d6c2e0e758ddbd3
brian <liar88828@gmail.com>
2023-06-02 15:16:12 +0700

    make login and register

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,21 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {
-	BrowserRouter as Router,
-	createBrowserRouter,
-	createRoutesFromElements,
-	Route,
-	RouterProvider,
-} from 'react-router-dom';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, } from 'react-router-dom';
 import { HomeScreen } from "./screen/HomeScreen.jsx";
+import { LoginScreen } from "./screen/LoginScreen.jsx";
+import { RegisterScreen } from "./screen/RegisterScreen.jsx";
 
 const router = createBrowserRouter(
 		createRoutesFromElements(
 				<Route path='/' element={ <App/> }>
 					<Route index={ true } path='/' element={ <HomeScreen/> }/>
-					{/*<Route path='/login' element={<LoginScreen />} />*/ }
-					{/*<Route path='/register' element={<RegisterScreen />} />*/ }
+					<Route path='/login' element={ <LoginScreen/> }/>
+					<Route path='/register' element={ <RegisterScreen/> }/>
 					{/*<Route path='' element={<PrivateRoute />}>*/ }
 					{/*	<Route path='/profile' element={<ProfileScreen />} />*/ }
 					{/*</Route>*/ }
@@ -25,11 +21,6 @@ const router = createBrowserRouter(
 );
 ReactDOM.createRoot(document.getElementById("root")).render(
 		<React.StrictMode>
-			{/*<Router>*/}
-
-				{/*<BrowserRouter>*/ }
-				<RouterProvider router={ router }/>
-				{/*</BrowserRouter>/*/ }
-			{/*</Router>*/}
+			<RouterProvider router={ router }/>
 		</React.StrictMode>,
 );
